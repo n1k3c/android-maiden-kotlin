18291355e6a744d30451c4c155b8fbc0ae586aa4
Nikola Curilovic <ncurilovic@gmail.com>
2017-05-24 03:31:29 +0200

    Add dependencies

--- app/src/main/java/mutiny/codes/maidenkotlin/MainActivity.kt
+++ app/src/main/java/mutiny/codes/maidenkotlin/MainActivity.kt
@@ -2,11 +2,16 @@ package mutiny.codes.maidenkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log.d
+import com.pawegio.kandroid.d
+import com.pawegio.kandroid.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
